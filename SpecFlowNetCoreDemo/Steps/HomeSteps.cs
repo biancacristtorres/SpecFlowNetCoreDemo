3ac18059dd6bc7305d9be95267a256b662623dc0
biancacristtorres <biancacristtorres@gmail.com>
2020-07-25 19:17:12 -0300

    porjeto de demo

---
+++ SpecFlowNetCoreDemo/Steps/HomeSteps.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowNetCoreDemo.Steps
+{
+    [Binding]
+    class HomeSteps
+    {
+    }
+}
---
+++ SpecFlowNetCoreDemo/Steps/LoginSteps.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SpecFlowNetCoreDemo.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecFlowNetCoreDemo.Steps
+{
+    [Binding]
+    public sealed class LoginSteps
+    {
+        LoginPage loginPage = null;
+
+        [Given(@"I lauch the application")]
+        public void GivenILauchTheApplication()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            loginPage = new LoginPage(webDriver);
+        }
+
+        [Given(@"I click login link")]
+        public void GivenIClickLoginLink()
+        {
+            loginPage.ClickLogin();
+        }
+
+        [Given(@"I enter the following details")]
+        public void GivenIEnterTheFollowingDetails(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            loginPage.Login((string)data.UserName, (string)data.Password);
+        }
+
+        [Given(@"I click login button")]
+        public void GivenIClickLoginButton()
+        {
+            loginPage.ClickLoginButton();
+        }
+
+        [Then(@"I should see Employee details link")]
+        public void ThenIShouldSeeEmployeeDetailsLink()
+        {
+            Assert.That(loginPage.IsEmployeeDetailsExist(), Is.True);
+        }
+
+    }
+}
